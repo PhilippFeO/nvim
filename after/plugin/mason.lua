40dc28ae607798b84721d376e0ab56ad84ff8e8d
PhilippFeO <email@example.com>
2023-12-19 21:23:31 +0100

    Add frame to LSP floating windows

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -82,3 +82,13 @@ require 'lspconfig'.pylsp.setup {
     }
   }
 }
+
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    -- Use a sharp border with `FloatBorder` highlights
+    border = { "🭽", "▔", "🭾", "▕", "🭿", "▁", "🭼", "▏" }, -- chars from kanagawa.nvim
+    -- add the title in hover float window
+    -- title = "hover"
+  }
+)
