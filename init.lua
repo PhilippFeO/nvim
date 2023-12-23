d06601c8315af27051f2cf6c9189026b1219adbe
PhilippFeO <email@example.com>
2023-12-23 22:34:25 +0100

    Change order of vim-markdown options

--- init.lua
+++ init.lua
@@ -66,11 +66,11 @@ require('lazy').setup({
     init = function()
       vim.g.vim_markdown_conceal = true
       vim.g.vim_markdown_folding_disabled = true
-      vim.g.vim_markdown_auto_insert_bullets = true
       vim.g.vim_markdown_toc_autofit = true   -- :Toc, :Tocv, :Toct
       vim.g.vim_markdown_strikethrough = true -- two ~ for strikethrough
       vim.g.vim_markdown_autowrite = true     -- write when following link
       vim.g.vim_markdown_follow_anchor = true -- follow anchored links
+      vim.g.vim_markdown_auto_insert_bullets = 0
       vim.g.vim_markdown_new_list_item_indent = 0
     end,
   },
