3e0d6210e09fd925da15a445bceaa4871ad16e10
PhilippFeO <email@example.com>
2023-12-23 23:03:02 +0100

    Add tex.lua (only links/references)

---
+++ compiler/tex.lua
@@ -0,0 +1,5 @@
+-- https://www.ejmastnak.com/tutorials/vim-latex/compilation/#compiler
+-- offers an extremly nice introduction into writing a compiler plugin for latex
+
+-- https://www.ejmastnak.com/tutorials/vim-latex/compilation/#quickfix
+-- documents useful settings for VimTeX
