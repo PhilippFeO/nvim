40971b12fc7becd1ceb27054eeaee8787420877d
PhilippFeO <email@example.com>
2023-12-24 09:55:15 +0100

    Convenience settings for help pages

--- ftplugin/help.lua
+++ ftplugin/help.lua
@@ -1,1 +1,5 @@
 vim.treesitter.start()
+
+-- These option, although set in plugin/options.lua aren't applied to help pages. Only god knows why.
+vim.o.relativenumber = true
+vim.opt.scrolloff = 8 -- s. plugin/options.lua
