dc485fadb9b3c6d6eecff6c9f16beefc64ef2795
PhilippFeO <email@example.com>
2023-12-27 21:48:51 +0100

    Change gitsign symbols

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,20 +1,14 @@
 require('gitsigns').setup {
-  signs = {
     -- Colors may be changed via the according highlight groupd, check :h gitsigns-highlight-groups
-    add          = { text = '│' },
-    change       = { text = '│' },
-    delete       = { text = '×' },
-    topdelete    = { text = '‾' },
+    add          = { text = '+' },
+    change       = { text = '~' },
+    delete       = { text = '󰧧' },
+    topdelete    = { text = '󰆴' },
     changedelete = { text = '~' },
     untracked    = { text = '┆' },
   },
-  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
   -- different highlight options
-  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
   -- Watch changes
-  watch_gitdir = {
     enable = true,
     interval = 1000,
     follow_files = true
