3026167640212a0a02627f61c5ae56b548eb3a7f
PhilippFeO <email@example.com>
2024-01-03 14:14:07 +0100

    Improve: Parse simple Python errors as they should
    Multiline Python errors without stacktrace are preprocessed via the script
    parse_python_error.sh (s. there for a desription). Due to this step,
    using Vim's errorformat is now much easier and more intuitively,
    yielding the correct result for the quickfix list. The errorfomat from
    'errorformat-multi-line' left the first 'Traceback'-line, which then appeared in the quickfix list.

--- compiler/python.lua
+++ compiler/python.lua
@@ -2,8 +2,8 @@
 
 -- Due to compatability reasons with older Vim versions better use CompilerSet than vim.bo.makeprg directly
 -- But since I won't publish my plugin, in principle I dont have to care
-vim.cmd.CompilerSet('makeprg=python3')
--- from `h errorformat`
-vim.cmd.CompilerSet([[errorformat=%C\ %.%#,%A\ \ File\ \"%f\"\\,\ line\ %l%.%#,%Z%[%^\ ]%\\@=%m]])
+vim.cmd.CompilerSet('makeprg=~/dotfiles/nvim/compiler/parse_python_error.sh')
+vim.cmd.CompilerSet([[errorformat=File\ \"%f\"\\,\ line\ %l\\,\ in\ <%.%#>\ %m]])
+
 
 vim.o.autowrite = true
