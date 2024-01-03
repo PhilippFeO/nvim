205fe1568375b56051a72a2293d6cc9728554a4e
PhilippFeO <email@example.com>
2024-01-03 14:37:01 +0100

    Add: Todo comment on python errors with stacktrace

--- compiler/python.lua
+++ compiler/python.lua
@@ -1,4 +1,5 @@
 -- TODO: setting current_compiler as described in `h current_compiler` doesn't work <22-12-2023>
+-- TODO: For parsing errors with stacktrace https://vi.stackexchange.com/questions/5110/quickfix-support-for-python-tracebacks might help <03-01-2024>
 
 -- Due to compatability reasons with older Vim versions better use CompilerSet than vim.bo.makeprg directly
 -- But since I won't publish my plugin, in principle I dont have to care
