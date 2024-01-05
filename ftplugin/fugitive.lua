e0fb477487046c94d9df35131f19c1d29aa30354
PhilippFeO <email@example.com>
2024-01-05 17:22:27 +0100

    Feat: Further 'git log' commands

--- ftplugin/fugitive.lua
+++ ftplugin/fugitive.lua
@@ -1,7 +1,15 @@
+-- ─── git push ──────────
 -- Git push origin HEAD
 vim.keymap.set('n', 'gp', '<Cmd>Git push origin HEAD<CR>', { buffer = true, desc = '[g]it [p]ush origin HEAD' })
 
-
+-- ─── git commit ──────────
 -- git commit --amend --no-edit
-vim.keymap.set('n', 'gcan', '<Cmd>Git commit --amend --no-edit<CR>',
+vim.keymap.set('n', 'gca', '<Cmd>Git commit --amend --no-edit<CR>',
     { buffer = true, desc = '[g]it [c]ommit --[a]mend --no-edit' })
+
+-- ─── git log ──────────
+-- git log --graph
+vim.keymap.set('n', 'gll', '<Cmd>Git log --graph<CR>', { buffer = true, desc = '[g]it [l]og --graph' })
+-- git log --graph --all -15
+vim.keymap.set('n', 'gla', '<Cmd>Git log --graph --all -15<CR>',
+    { buffer = true, desc = '[g]it [l]og --graph --[a]ll -15' })
