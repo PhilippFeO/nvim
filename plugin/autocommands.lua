195b57008c11df187cfec2bf58f7e1a0e56fc8ef
PhilippFeO <email@example.com>
2024-01-05 17:25:47 +0100

    Remove old/unused autocommands

--- plugin/autocommands.lua
+++ plugin/autocommands.lua
@@ -2,26 +2,9 @@ local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
 
--- FOLDS
--- ─────
--- Save and load folds
--- autocmd('BufWinLeave', {
---     pattern = '*.*',
---     command = 'mkview',
---     desc = 'Save folds.'
--- })
-
--- Shorter with VimScript:
---      au BufWinEnter *.* silent! loadview
--- autocmd('BufWinEnter', {
---     pattern = '*',
---     command = 'silent! loadview',
---     desc = "Load saved folds."
--- })
-
 -- Disable signcolumn in my wiki (I won't commit it and need no git-signs information)
 autocmd('BufWinEnter', {
-    group = vim.api.nvim_create_augroup('Disable-Signcolumn', { clear = true }),
+    group = augroup('Disable-Signcolumn', { clear = true }),
     pattern = '/home/philipp/wiki/*',
     callback = function()
         vim.wo.signcolumn = 'no'
@@ -33,7 +16,7 @@ autocmd('BufWinEnter', {
 -- TODO: Edit kanagawa.nvim or highlight groups using kanagawa's palette because highlight group colors are ugly
 -- TODO: Which highlight group is used by vim.lsp.buf.document_highlight()?
 autocmd('CursorHold', {
-    group = vim.api.nvim_create_augroup('Document-Highlight-Normal', { clear = true }),
+    group = augroup('Document-Highlight-Normal', { clear = true }),
     callback = function()
         vim.lsp.buf.document_highlight()
     end,
@@ -41,14 +24,14 @@ autocmd('CursorHold', {
 })
 -- I = Insert Mode
 autocmd('CursorHoldI', {
-    group = vim.api.nvim_create_augroup('Document-Highlight-Insert', { clear = true }),
+    group = augroup('Document-Highlight-Insert', { clear = true }),
     callback = function()
         vim.lsp.buf.document_highlight()
     end,
     desc = 'Identifier highlighting in Insert Mode on CursorHold'
 })
 autocmd('CursorMoved', {
-    group = vim.api.nvim_create_augroup('Document-Highlight-Clear', { clear = true }),
+    group = augroup('Document-Highlight-Clear', { clear = true }),
     callback = function()
         vim.lsp.buf.clear_references()
     end,
