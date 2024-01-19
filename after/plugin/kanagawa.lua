a5cb709685bf2d7060890b562b1c0595b4760402
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-01-19 20:56:20 +0100

    🎨 Update(kanagawa): Background of NormalFloat more distinguishable
    S. Comments in Commit for explanation.

--- after/plugin/kanagawa.lua
+++ after/plugin/kanagawa.lua
@@ -25,11 +25,12 @@ require('kanagawa').setup({
     -- 'overrides' is for highlight groups
     overrides = function(colors)
         local theme = colors.theme
+        local palette = colors.palette
         return {
             -- Example syntax for Treesitter related highlight groups
             -- ['@text.diff.add'] = { fg = "blue" },
 
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },         -- add `blend = vim.o.pumblend` to enable transparency
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
             PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
             PmenuSbar = { bg = theme.ui.bg_m1 },
             PmenuThumb = { bg = theme.ui.bg_p2 },
@@ -42,7 +43,10 @@ require('kanagawa').setup({
             -- LspReferenceWrite = { bg = colors.palette.dragonBlack6, bold = true },
 
             -- Settings for floating windows
-            NormalFloat = { bg = "none" }, -- try #54546D=sumiInk4 from kanagawa
+            -- `NormalFloat` is used for LSP-Documentation (the window displaying the part behind `documentation` of a `nvim-cmp` source)
+            -- The default/recommened value `"none"` is the same as in a normal buffer => It blends into main window which makes it hard to distinguish between LSP-Documentation and Code. I've set it to the same value as the Pmenu displaying all results.
+            -- IDEA: background of {Lazy, Mason}Normal also possible
+            NormalFloat = { bg = theme.ui.bg_p2 },
             FloatBorder = { bg = "none" },
             FloatTitle = { bg = "none" },
 
