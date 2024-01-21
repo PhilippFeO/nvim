4b7b2083e6aa3f4ad0740acb460cbb99d59a97e5
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-01-21 12:57:13 +0100

    ♻️  Refactor(init): Try 'ft' option of lazy.nvim
    Sometimes it works, sometimes not.

--- init.lua
+++ init.lua
@@ -77,6 +77,7 @@ require('lazy').setup({
       vim.g.vim_markdown_auto_insert_bullets = 0
       vim.g.vim_markdown_new_list_item_indent = 0
     end,
+    ft = 'markdown'
   },
 
   -- Becomes active after `:h timeoutlen`
@@ -123,6 +124,7 @@ require('lazy').setup({
         -- toc = require("wiki.ui_select").toc,
       }
     end,
+    -- ft = { 'markdown', 'wiki' } -- doesn't work
   },
 
   -- ─── Snippets ──────────
