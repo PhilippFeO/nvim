6c1ad02e673b0decee37717b1184ca3f430cd85f
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-01-30 20:40:18 +0100

    🎨 Update(ft/lua): Keymap for opening help
    I regularly link help pages in my Neovim-Config.

---
+++ ftplugin/lua.lua
@@ -0,0 +1,3 @@
+-- analogous to ftplugin/markdown.lua
+vim.keymap.set('n', '<M-k>', '"hyi`:tab <C-r>h<CR>',
+    { buffer = true, desc = 'Open help page reference in Wiki (lua only: <M-k> not K/<S-k>)' })
