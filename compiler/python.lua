193f7644ac8363eb2575c0b44e7cabc39ebb8314
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-01-30 20:41:08 +0100

    💡 Add/Update Comments(compiler/python)

--- compiler/python.lua
+++ compiler/python.lua
@@ -4,7 +4,10 @@
 -- Due to compatability reasons with older Vim versions better use CompilerSet than vim.bo.makeprg directly
 -- But since I won't publish my plugin, in principle I dont have to care
 vim.cmd.CompilerSet('makeprg=~/dotfiles/nvim/compiler/parse_python_error.sh')
+-- TODO: Good idea to use `h CompilerSet` for errorformat? <30-01-30-0130-01-2024
+-- I barely understand `h CompilerSet`
 vim.cmd.CompilerSet([[errorformat=File\ \"%f\"\\,\ line\ %l\\,\ in\ <%.%#>\ %m]])
+-- vim.bo.errorformat = [[File\ \"%f\"\\,\ line\ %l\\,\ in\ <%.%#>\ %m]]
 
 
 vim.o.autowrite = true
