2783c1ffcdb765c4bc81eddf01b7ba03b18c955f
Philipp Rost <philipp.rost@dlr.de>
2024-03-04 20:08:58 +0100

    🛰️🚀📡🌌☄️

--- plugin/autocommands.lua
+++ plugin/autocommands.lua
@@ -15,29 +15,29 @@ autocmd('BufWinEnter', {
 -- ─── LSP Autocommands ──────────
 -- TODO: Edit kanagawa.nvim or highlight groups using kanagawa's palette because highlight group colors are ugly
 -- TODO: Which highlight group is used by vim.lsp.buf.document_highlight()?
-autocmd('CursorHold', {
-    group = augroup('Document-Highlight-Normal', { clear = true }),
-    callback = function()
-        vim.lsp.buf.document_highlight()
-    end,
-    desc = 'Identifier highlighting on CursorHold'
-})
--- I = Insert Mode
-autocmd('CursorHoldI', {
-    group = augroup('Document-Highlight-Insert', { clear = true }),
-    callback = function()
-        vim.lsp.buf.document_highlight()
-    end,
-    desc = 'Identifier highlighting in Insert Mode on CursorHold'
-})
-autocmd('CursorMoved', {
-    group = augroup('Document-Highlight-Clear', { clear = true }),
-    callback = function()
-        vim.lsp.buf.clear_references()
-    end,
-    desc = 'Remove identifier highlighting on CorsorMoved'
-})
-
+-- autocmd('CursorHold', {
+--     group = augroup('Document-Highlight-Normal', { clear = true }),
+--     callback = function()
+--         vim.lsp.buf.document_highlight()
+--     end,
+--     desc = 'Identifier highlighting on CursorHold'
+-- })
+-- -- I = Insert Mode
+-- autocmd('CursorHoldI', {
+--     group = augroup('Document-Highlight-Insert', { clear = true }),
+--     callback = function()
+--         vim.lsp.buf.document_highlight()
+--     end,
+--     desc = 'Identifier highlighting in Insert Mode on CursorHold'
+-- })
+-- autocmd('CursorMoved', {
+--     group = augroup('Document-Highlight-Clear', { clear = true }),
+--     callback = function()
+--         vim.lsp.buf.clear_references()
+--     end,
+--     desc = 'Remove identifier highlighting on CorsorMoved'
+-- })
+--
 
 -- CODEIUM
 -- ───────
