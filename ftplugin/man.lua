623fd430047d7f5b021bfbf627dd271198e3b11d
pfeo <125358469+PhilippFeO@users.noreply.github.com>
2024-03-12 15:25:30 +0100

    🐛 Fix: relativenumber in man pages

--- ftplugin/man.lua
+++ ftplugin/man.lua
@@ -1,3 +1,3 @@
 -- These option, although set in plugin/options.lua aren't applied to help pages. Only god knows why.
-vim.bo.relativenumber = true
+vim.o.relativenumber = true
 vim.opt.scrolloff = 8 -- s. plugin/options.lua
