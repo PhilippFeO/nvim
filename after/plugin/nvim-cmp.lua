a36e74f2a1dd8dae5adbfcda403a204ffb404cb1
pfeo <125358469+PhilippFeO@users.noreply.github.com>
2024-03-26 08:26:52 +0100

    🛰️🚀📡🌌☄️

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -74,7 +74,7 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<C-y>'] = cmp.mapping.confirm {
+    ['<C-<CR>>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     },
