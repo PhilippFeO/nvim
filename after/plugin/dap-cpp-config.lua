e9bcdd2ab4dafc5392c4f88dfbb2b172ac64f60b
pfeo <125358469+PhilippFeO@users.noreply.github.com>
2024-04-16 07:02:55 +0200

    🛰️🚀📡🌌☄️  16.04.2024 07:02

--- after/plugin/dap-cpp-config.lua
+++ after/plugin/dap-cpp-config.lua
@@ -30,6 +30,7 @@ dap.configurations.cpp = {
     },
 }
 
+-- ~/.cache/nvim/cache
 require('dap').set_log_level('TRACE')
 
 -- ─── Cpptools ──────────
@@ -52,7 +53,7 @@ dap.configurations.cpp = {
         end,
         cwd = '${workspaceFolder}',
         stopAtEntry = true,
-        -- externalConsole = true,
+        externalConsole = true,
         -- gdb pretty printing
         setupCommands = {
             {
@@ -82,9 +83,20 @@ dap.configurations.cpp = {
             '10',
             '-t',
             '1', -- Sequentiell
+            '--alongtrack',
             '--aux-cloud $(find . -name "*L2__CLOUD*.nc" | sort -n | tail -n 1)',
             '--aux-o3 $(find . -name "*L2__O3*.nc" | sort -n | tail -n 1)',
         },
+        --[[
+./upas-l2
+ --so2 /home/sentinel5p-data/archive/L1B/2024/03/15/irradiance/OFFL/S5P_OFFL_L1B_IR_UVN_20240315T062230_20240315T080401_33268_03_020100_20240315T095524.nc /home/sentinel5p-data/archive/L1B/2024/03/15/33270/OFFL/S5P_OFFL_L1B_RA_BD3_20240315T094531_20240315T112702_33270_03_020100_20240315T131648.nc
+ -b 2000
+ -n 10
+ -t 12
+ --alongtrack
+ --aux-cloud $(find . -name "*L2__CLOUD*.nc" | sort -n | tail -n 1)
+ --aux-o3 $(find . -name "*L2__O3*.nc" | sort -n | tail -n 1)`
+--]]
         cwd = vim.fn.expand('~/proj/upas-s5p/UPAS-L2/build/verbose/'),
         stopAtEntry = true,
         -- externalConsole = true,
