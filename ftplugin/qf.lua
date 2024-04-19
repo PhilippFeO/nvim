7e54ae3e1c476cedc8cc5df14bc1f4a2aa8e3b34
pfeo <125358469+PhilippFeO@users.noreply.github.com>
2024-04-19 21:20:09 +0200

    🛰️🚀📡🌌☄️  19.04.2024 21:20

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -1,4 +1,4 @@
--- qf = quickfix
+-- Events: QuickFixCmdPre, QuickFixCmdPost
 
 -- Window local highlighting; may be overwritten by namespaces
 --      Background 1: Highlights are global, ie. `vim.api.nvim_set_hl(0, "LineNr", { fg = '#ffffff' })` triggered in a qf-window changes the Line numbering in every other window. The `winhighlight` option offers a way around this.
@@ -8,10 +8,12 @@ local cp = colors.palette
 vim.api.nvim_set_hl(0, 'MyLineNr', { fg = cp.sakuraPink })
 vim.o.winhighlight = 'LineNr:MyLineNr'
 
+
 vim.api.nvim_create_autocmd(
     'FileType', {
         group = vim.api.nvim_create_augroup('Quickfix-List-Window', { clear = true }),
         pattern = 'qf',
+        -- Alternative: :botright cwindow
         command = vim.cmd.wincmd('J'),
         desc = 'Spread Quickfix-List over full breadth when using splits'
     }
--- ftplugin/sh.lua
+++ ftplugin/sh.lua
@@ -1,2 +1,5 @@
 -- Loads RUNTIMEPATH/compiler/python.lua
-vim.cmd.compiler('bash')
+-- vim.cmd.compiler('bash')
+
+vim.o.makeprg = 'bash'
+vim.o.errorformat = [[%f: line %l: %m]]
