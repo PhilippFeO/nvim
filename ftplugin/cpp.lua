bd8d3f5901ce3cf1d118ea948fa3459c869f3274
pfeo <125358469+PhilippFeO@users.noreply.github.com>
2024-04-19 14:01:31 +0200

    ✨ Feat(ft/cpp): errorformat

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,4 +1,17 @@
-vim.o.makeprg = [[make --no-print-directory -C ../build/]]
+local release = false
+if release then
+    vim.o.makeprg = [[make --no-print-directory -C ~/proj/upas-s5p/UPAS-L2/build/release]]
+else
+    vim.o.makeprg = [[make --no-print-directory -C ~/proj/upas-s5p/UPAS-L2/build/verbose]]
+end
 
-vim.keymap.set('n', '<Leader>cm', '<Cmd>!cd ../build && cmake ../src<CR>',
+-- Verzeichnisabhängige Pfade (bspw. für S4-UPAS: Mit if-else und vim.fn.expand('%:p') arbeiten)
+
+-- Vim is PWD aware when listing the file path in the quickfix list, ie. only the missing part is displayed (fi. /localhome/rost_ph/proj/upas-s5p/ is skipped)
+-- Pfade werden relativ zum PWD angezeigt und verarbeitet, Nur '/localhome/' abschneiden geht also nicht
+-- Pfad zum Arbeitsverzeichnis eingefügt, da dann ausschließlich UPAS-Dateien „getroffen“ werden (aber nicht die Bibliothektsdateien wie dlib/matrix.h, etc.)
+vim.o.errorformat = [[/localhome/rost_ph/proj/upas-s5p/UPAS-L2/src/%f:%l:%c: %m]]
+
+vim.keymap.set('n', '<Leader>cm',
+    '<Cmd>!cd ../build && cmake ../src && cd -<CR>',
     { desc = '[cm]ake ../src in build/' })
