b3e186943c9885da2a3989f8e9dece77e16aa79a
pfeo <125358469+PhilippFeO@users.noreply.github.com>
2024-04-23 20:25:56 +0200

    🛰️🚀📡🌌☄️  23.04.2024 20:25

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -6,14 +6,16 @@ if string.find(vim.fn.expand('%:p'), 'UPAS-L2') then
     else
         vim.o.makeprg = [[make --no-print-directory -C ~/proj/upas-s5p/UPAS-L2/build/verbose]]
     end
+    -- Vim is PWD aware when listing the file path in the quickfix list, ie. only the missing part is displayed (fi. /localhome/rost_ph/proj/upas-s5p/ is skipped)
+    -- Pfade werden relativ zum PWD angezeigt und verarbeitet, Nur '/localhome/' abschneiden geht also nicht
+    -- Pfad zum Arbeitsverzeichnis eingefügt, da dann ausschließlich UPAS-Dateien „getroffen“ werden (aber nicht die Bibliothektsdateien wie dlib/matrix.h, etc.)
+    vim.o.errorformat = [[/localhome/rost_ph/proj/upas-s5p/UPAS-L2/src/%f:%l:%c: %m]]
 else
     vim.o.makeprg = [[make --no-print-directory -C ../build]]
+    -- %:p:h Path without file (and trailing /)
+    vim.o.errorformat = vim.fn.expand('%:p:h') .. '/%f:%l:%c: %m'
 end
 
--- Vim is PWD aware when listing the file path in the quickfix list, ie. only the missing part is displayed (fi. /localhome/rost_ph/proj/upas-s5p/ is skipped)
--- Pfade werden relativ zum PWD angezeigt und verarbeitet, Nur '/localhome/' abschneiden geht also nicht
--- Pfad zum Arbeitsverzeichnis eingefügt, da dann ausschließlich UPAS-Dateien „getroffen“ werden (aber nicht die Bibliothektsdateien wie dlib/matrix.h, etc.)
-vim.o.errorformat = [[/localhome/rost_ph/proj/upas-s5p/UPAS-L2/src/%f:%l:%c: %m]]
 
 vim.keymap.set('n', '<Leader>cm',
     '<Cmd>!cd ../build && cmake ../src && cd -<CR>',
