485f6df828613bb12b7fa9db74916388cc818751
P_FeO <125358469+PhilippFeO@users.noreply.github.com>
2024-05-04 09:57:45 +0200

    🎨 Update(dap): expand path of debugpy venv

--- after/plugin/nvim-dap-python.lua
+++ after/plugin/nvim-dap-python.lua
@@ -2,8 +2,8 @@ local dap_python = require('dap-python')
 -- setup according to https://github.com/mfussenegger/nvim-dap-python
 -- The debugger will automatically pick-up another virtual environment if it is activated before neovim is started.
 -- TODO: Using an environment without debugpy also works. I thought it wouldn't. <26-026-01-2024
--- dap_python.setup(vim.fn.expand('$VIRTUAL_ENV'))
+-- dap_python.setup(vim.fn.expand(os.getenv('VIRTUAL_ENV')))
 
-dap_python.setup('~/.venv/debugpy/bin/python')
+dap_python.setup(vim.fn.expand('~/.venv/debugpy/bin/python'))
 
 dap_python.test_runner = 'pytest'
