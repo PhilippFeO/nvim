b8f8e93cb0889ce5fd898b84306cecf119e75f84
pfeo <125358469+PhilippFeO@users.noreply.github.com>
2024-05-06 08:09:05 +0200

    🛰️🚀📡🌌☄️  06.05.2024 08:09

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,5 +1,8 @@
 vim.cmd.compiler('cpp')
 
+vim.keymap.set('n', '<Leader>mm', '<Cmd>w | make -j8 | cwindow 12 | wincmd k<CR>',
+    { desc = 'make/compile/execute current file with -j8' })
+
 vim.keymap.set('n', '<Leader>cm',
     '<Cmd>!cd ../build && cmake ../src && cd -<CR>',
     { desc = '[cm]ake ../src in build/' })
