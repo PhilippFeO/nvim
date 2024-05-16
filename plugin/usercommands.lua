d2732675109f49313ab9cdecaebe754889a1d048
pfeo <125358469+PhilippFeO@users.noreply.github.com>
2024-05-16 07:37:59 +0200

    ✨ Feat(userC): Toggle light/dark version of colorscheme

--- plugin/usercommands.lua
+++ plugin/usercommands.lua
@@ -3,4 +3,14 @@ user_command('CorrectMD', 'set hlsearch | %s/` \\(.\\{-}\\)`/ `\\1`/gc', {
     desc = 'WORD` CODE` -> WORD `CODE`',
 })
 
-vim.api.nvim_create_user_command('KL', 'colorscheme kanagawa-lotus', { desc = ':colorscheme kanagawa-lotus' })
+-- By executing :KL the light/dark version of the kanagawa colorscheme is toggled
+local kanagawa_toggle = true
+vim.api.nvim_create_user_command('KL', function(_)
+    if kanagawa_toggle then
+        kanagawa_toggle = not kanagawa_toggle
+        vim.cmd('colorscheme kanagawa-lotus')
+    else
+        kanagawa_toggle = not kanagawa_toggle
+        vim.cmd('colorscheme kanagawa')
+    end
+end, { desc = ':colorscheme kanagawa-lotus' })
