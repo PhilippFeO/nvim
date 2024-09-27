4f19fe80e8c67f9253570832be769f59d147f45d
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-09-27 07:51:11 +0200

    feat(octo): ✨ Search mentionalbe users as Assignees

--- after/plugin/octo.lua
+++ after/plugin/octo.lua
@@ -3,3 +3,8 @@ local nmap = require 'utils'.nmap('Octo')
 nmap('<Leader>ic', '<Cmd>Octo issue create<CR>', '[i]ssue [c]reate')
 nmap('<Leader>ie', ':Octo issue edit ', '[i]ssue [e]dit')
 nmap('<Leader>il', '<Cmd>Octo issue list<CR>', '[i]ssue [l]ist')
+
+
+require('octo').setup({
+  users = "mentionable",
+})
