fab3b97f4170a45b878d284120e0a6fac38dc86e
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-11-13 08:21:25 +0100

    feat: ✨ Keymap for new MD buffer (for Octo)

--- after/plugin/octo.lua
+++ after/plugin/octo.lua
@@ -1,6 +1,7 @@
 local nmap = require 'utils'.nmap('Octo')
 
 nmap('<Leader>ic', '<Cmd>Octo issue create<CR>', '[i]ssue [c]reate')
+nmap('<Leader>ib', '<Cmd>tabnew | set ft=markdown<CR>', '[i]ssue [b]uffer')
 nmap('<Leader>ie', ':Octo issue edit ', '[i]ssue [e]dit')
 nmap('<Leader>il', '<Cmd>Octo issue list<CR>', '[i]ssue [l]ist')
 
