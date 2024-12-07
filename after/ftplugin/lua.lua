c337205a148793d80a3c98fd564c894d2b844c44
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-12-07 09:59:57 +0100

    🏡 07.12.2024 09:59

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -3,3 +3,4 @@ vim.keymap.set('n', '<M-k>', '"hyi`:tab <C-r>h<CR>',
     { buffer = true, desc = 'Open help page reference in Wiki (lua only: <M-k> not K/<S-k>)' })
 
 vim.bo.tabstop = 2
+vim.opt_local.shiftwidth = 2
