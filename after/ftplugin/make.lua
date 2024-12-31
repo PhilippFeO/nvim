0ef7d71b814c90a1adfcae034cc64b82eb5dfae4
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-12-31 12:58:41 +0100

    chore(ftpluging): 🚚 Move list-opt completely into ftplugin/make.lua

--- after/ftplugin/make.lua
+++ after/ftplugin/make.lua
@@ -1,3 +1,5 @@
+-- To demystify white space in files, fi. tab vs. space
+vim.opt.list = true
 -- Show TAB as '>---'
 -- Show leading multiple (4) spaces as '---<'
 vim.opt_local.listchars = 'tab:>-,leadmultispace:---<'
