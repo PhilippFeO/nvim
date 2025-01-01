5fd63ba401d8915c00a3fd96bb0eddba131451a8
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2025-01-01 16:07:35 +0100

    feat: 🚀 Ignore logging output within errorformat

--- compiler/python.lua
+++ compiler/python.lua
@@ -1,9 +1,16 @@
 -- TODO: Only if Makefile exists <13-05-2024>
+
+-- `%.%#` == `.*`
+
 vim.cmd('CompilerSet makeprg=make')
 -- Ignore following pytest line (emerges if test is decorated with '@pytest.mark.skip')
 -- SKIPPED [1] tests/transfer_files_test.py|77| unconditional skip
 -- ^=: prepend to string
 -- %-G: ignore
 vim.cmd('CompilerSet errorformat^=%-GSKIPPED%.%#')
+-- Ignore logging.LOGLEVEL output, fi:
+-- [INFO:  01.01.2025  11:56:50  add_description]  Orientation: Horizontal (normal)
+vim.cmd('CompilerSet errorformat^=%-G[INFO:%.%#')
+vim.cmd('CompilerSet errorformat^=%-G[WARNING:%.%#')
 -- s. `h errorformat-multi-line`
 vim.cmd [[CompilerSet errorformat+=%C\ %.%#,%A\ \ File\ \"%f\"\\,\ line\ %l%.%#,%Z%[%^\ ]%\\@=%m]]
