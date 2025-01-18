d833d16b833fe79607d8a35415413a205cd9f95a
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2025-01-18 21:47:24 +0100

    🏡 18.01.2025 21:47

--- compiler/python.lua
+++ compiler/python.lua
@@ -13,5 +13,8 @@ vim.cmd('CompilerSet errorformat^=%-GSKIPPED%.%#')
 vim.cmd('CompilerSet errorformat^=%-G[INFO:%.%#')
 vim.cmd('CompilerSet errorformat^=%-G[WARNING:%.%#')
 vim.cmd('CompilerSet errorformat^=%-G2025%.%#')
+-- Ignore Output starting with a number
+-- used within open_meteo
+vim.cmd('CompilerSet errorformat^=%-G%*[0-9]%.%#')
 -- s. `h errorformat-multi-line`
 vim.cmd [[CompilerSet errorformat+=%C\ %.%#,%A\ \ File\ \"%f\"\\,\ line\ %l%.%#,%Z%[%^\ ]%\\@=%m]]
