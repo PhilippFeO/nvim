71bf7d1ae8b0df49006d36176e698fe9991a32a5
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2025-10-08 07:40:37 +0200

    🏡 08.10.2025 07:40

--- after/plugin/dap-configs/python-tagebuch.lua
+++ after/plugin/dap-configs/python-tagebuch.lua
@@ -1,14 +1,28 @@
-local render_diary_template = {
+local open_diary_entry = {
   name = 'Tagebuch: Erstelle und öffne Eintrag',
   request = 'launch',
   type = 'python',
-  program = vim.fn.expand '~/.tagebuch/tagebuch/render_diary_template.py',
+  program = vim.fn.expand '~/.tagebuch/tagebuch/open_diary_entry.py',
   cwd = vim.fn.expand '~/.tagebuch/',
   args = { vim.fn.expand '~/.tagebuch/2025/10-Oktober/2025-10-03/' },
 }
 
+local past_entries = {
+  name = 'Tagebuch: Öffne vergangene Einträge',
+  request = 'launch',
+  type = 'python',
+  program = vim.fn.expand '~/.tagebuch/tagebuch/past.py',
+  -- program = 'tagebuch',
+  cwd = vim.fn.expand '~/.tagebuch',
+  args = { '2025-10-03' },
+  env = {
+    PYTHONPATH = "/home/philipp/.tagebuch/.venv/tagebuch/bin/python3",
+  }
+}
+
 return {
   configs = {
-    render_diary_template,
+    open_diary_entry,
+    -- past_entries,
   },
 }
--- after/plugin/nvim-dap-python.lua
+++ after/plugin/nvim-dap-python.lua
@@ -1,9 +1,11 @@
 local dap_python = require('dap-python')
 -- setup according to https://github.com/mfussenegger/nvim-dap-python
 -- The debugger will automatically pick-up another virtual environment if it is activated before neovim is started.
--- TODO: Using an environment without debugpy also works. I thought it wouldn't. <26-026-01-2024
--- dap_python.setup(vim.fn.expand(os.getenv('VIRTUAL_ENV')))
 
-dap_python.setup(vim.fn.expand('~/.venv/debugpy/bin/python'))
+-- Either project specific Venv with debugpy installed or ...
+dap_python.setup(vim.fn.expand(os.getenv('VIRTUAL_ENV') .. '/bin/python3'))
+
+-- debugpy specific Venv, ie. a Venv with only debugpy installed
+-- dap_python.setup(vim.fn.expand('~/.venv/debugpy/bin/python'))
 
 dap_python.test_runner = 'pytest'
