dcb79b011ff2314e8e0fa89c63bbe78c1c35a4fe
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2025-10-09 10:03:28 +0200

    🏡 09.10.2025 10:03

--- after/plugin/dap-configs/python-tagebuch.lua
+++ after/plugin/dap-configs/python-tagebuch.lua
@@ -2,7 +2,7 @@ local open_diary_entry = {
   name = 'Tagebuch: Erstelle und öffne Eintrag',
   request = 'launch',
   type = 'python',
-  program = vim.fn.expand '~/.tagebuch/tagebuch/open_diary_entry.py',
+  program = vim.fn.expand '~/.tagebuch/tagebuch/__main__.py',
   cwd = vim.fn.expand '~/.tagebuch/',
   args = { vim.fn.expand '~/.tagebuch/2025/10-Oktober/2025-10-03/' },
 }
@@ -13,7 +13,7 @@ local past_entries = {
   type = 'python',
   program = vim.fn.expand '~/.tagebuch/tagebuch/__main__.py',
   -- program = 'tagebuch',
-  cwd = vim.fn.expand '~/.tagebuch',
+  cwd = vim.fn.expand '~/.tagebuch/',
   args = { '--past', '2025-10-03' },
   -- env = {
   --   PYTHONPATH = "/home/philipp/.tagebuch/.venv/tagebuch/bin/python3",
