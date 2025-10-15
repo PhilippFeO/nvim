86eca053d5792b640d958b59981278bfbbec1d69
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2025-10-15 14:04:46 +0200

    feat(treesitter): Fix branch master

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,5 +7,8 @@ return {
   config = function()
     pcall(require('nvim-treesitter.install').update { with_sync = true })
   end,
+  -- 2025-10-15: master is frozen, development happens on main.
+  -- Currently, I don't have the time and motivation to switch since it's a rewrite and my config doesn't work anymore.
+  branch = 'master',
   -- commit = '7d0b475'
 }
