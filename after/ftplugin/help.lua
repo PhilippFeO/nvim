1162d035b1450833e69ba83030cb5c1021d4cd15
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2025-10-19 11:17:30 +0200

    🏡 19.10.2025 11:17

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -3,3 +3,8 @@ vim.treesitter.start()
 -- These option, although set in plugin/options.lua aren't applied to help pages. Only god knows why.
 vim.o.relativenumber = true
 vim.opt.scrolloff = 8 -- s. plugin/options.lua
+
+vim.keymap.set('n', 'K', '<C-]>', {
+    desc = 'Jump to tag under cursor',
+    remap = true,
+})
