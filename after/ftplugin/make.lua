3d1f753ee7d32aac94a3a21b5f2737afb03e1296
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2025-10-22 10:42:46 +0200

    chore(make): Show Tabs as <->

--- after/ftplugin/make.lua
+++ after/ftplugin/make.lua
@@ -2,4 +2,4 @@
 vim.opt_local.list = true
 -- Show TAB as '>---'
 -- Show leading multiple (4) spaces as '---<'
-vim.opt_local.listchars = 'tab:>-,leadmultispace:---<'
+vim.opt_local.listchars = 'tab:<->,leadmultispace:---<'
