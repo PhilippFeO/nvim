bdb06161251049d5be7b7fe2bc6f37e4b3486989
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2026-04-13 21:18:57 +0200

    chore(help): Show line number (instead of 0)

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,7 +1,8 @@
 vim.treesitter.start()
 
 -- These option, although set in plugin/options.lua aren't applied to help pages. Only god knows why.
-vim.o.relativenumber = true
+vim.opt.relativenumber = true
+vim.opt.number = true
 vim.opt.scrolloff = 8 -- s. plugin/options.lua
 
 vim.keymap.set('n', 'K', '<C-]>', {
