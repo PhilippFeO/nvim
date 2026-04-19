1e33e4cc9d0601024894c7b02a662692d763c5cb
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2026-04-19 16:42:45 +0200

    feat(neogit): Add action to BranchPopup
    Do a final cleanup after a PR war merged.

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -2,6 +2,62 @@
 -- y: Öffne alle Revisions (Zweige (remote, loka), Tags, Stashes, …)
 -- „Popups“ öffnen sich und bieten weitere Optionen für Befehl an.
 
+local neogit = require('neogit')
+local ngit = require("neogit.lib.git")
+-- local ngit = neogit.lib.git
+-- local b = ngit.branch.current()
+-- 'connected' Upstream branch
+-- print(vim.inspect(ngit.branch.upstream(b)))
+-- print(vim.inspect(ngit.cli.branch.delete.name('test')))
+-- print(vim.inspect(ngit.cli.branch.delete.remotes.name('test').call({ await = true })))
+
+-- Execute after PR was merged
+--  - Fetches new main
+--  - checks it out
+--  - deletes feature branch it's remote counterpart
+local function final_cleanup(_)
+  local remote = ngit.branch.upstream_remote()
+  local current_branch = ngit.branch.current()
+  local main = ngit.branch.base_branch()
+  local upstream_main = ngit.branch.upstream(main)
+  local result
+  if remote and current_branch and main and upstream_main then
+    -- TODO(Philipp): Add --prune <19-04-2026>
+    print('Fetch from "' .. remote .. main .. '"')
+    ngit.fetch.fetch(remote, main)
+    -- Oder `checkout(main)`?
+    -- upstream_main, if pushing to main is permitted
+    print('Checkout ' .. upstream_main)
+    ngit.branch.checkout(upstream_main)
+    -- delete: append -d
+    -- remotes: append -r
+    -- => git branch -d -r NAME
+    result = ngit.cli.branch.delete.name(current_branch).call({ await = true })
+    if result and result:success() or false then
+      result = ngit.cli.branch.delete.remotes.name(remote .. '/' .. current_branch).call({ await = true })
+    end
+  end
+
+  return result and result:success() or false
+
+  -- -- You can access the popup state (enabled flags) like so:
+  -- local cli_args = popup:get_arguments()
+  --
+  -- -- You can use Neogit's git abstraction for many common operations
+  -- -- local git = require("neogit.lib.git")
+  --
+  -- -- The input library provides some helpful interfaces for getting user
+  -- -- input
+  -- local input = require("neogit.lib.input")
+  -- local user_input = input.get_user_input("User-specified free text for the action")
+  --
+  -- vim.notify(
+  --   "Hello from my custom action!\n"
+  --   .. "CLI args: `" .. table.concat(cli_args, " ") .. "`\n"
+  --   .. "User input: `" .. user_input .. "`")
+end
+
+
 require('neogit').setup({
   cmd = "Neogit",
   kind = "floating",
@@ -34,6 +90,13 @@ require('neogit').setup({
       folded = false,
       hidden = false,
     },
+  },
+  builders = {
+    NeogitBranchPopup = function(builder)
+      builder
+          :new_action_group('My Actions')
+          :action('f', 'Final Cleanup', final_cleanup)
+    end,
   },
 })
 
