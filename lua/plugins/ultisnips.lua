e3a696cdd11a50dd0dd6bac79d788c5441bce41a
Philipp Rost <rphilipp@gi-netz.de>
2026-05-12 09:26:53 +0200

    chore(ultisnips): More general path to snippet files

--- lua/plugins/ultisnips.lua
+++ lua/plugins/ultisnips.lua
@@ -13,7 +13,7 @@ return {
 
         -- setting absolute path prevents UltiSnips from scanning my whole <runtimepath> for the <schnipsel> directory
         -- "UltiSnips" needed for vim-snippets/UltiSnips/ directory
-        vim.g.UltiSnipsSnippetDirectories = { "UltiSnips", vim.fn.expand("~/dotfiles/nvim/schnipsel/") }
+        vim.g.UltiSnipsSnippetDirectories = { "UltiSnips", vim.fn.stdpath('config') .. '/schnipsel' }
 
         -- Keymaps
         -- ───────
