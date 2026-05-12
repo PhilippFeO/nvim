1c757345fd8c42a4acfe3b9820ccba36ff8d8833
Philipp Rost <rphilipp@gi-netz.de>
2026-05-12 08:37:18 +0200

    chore(octo): Loading is work machine agnostic

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -12,5 +12,8 @@ return {
   keys = {
     -- { '<Leader>o', '<Cmd>Octo<CR>', desc = 'Octo: [o]cto-Befehlsübersicht' }
     { '<Leader>iu', '<Cmd>Octo issue url<CR>', desc = 'Octo: [i]ssue [u]rl' },
-  }
+  },
+  -- 2026-05-09: `gh` missing on Windows
+  -- See also after/plugins/octo.lua: setup() call commented out
+  enabled = not IS_WORK_MACHINE,
 }
