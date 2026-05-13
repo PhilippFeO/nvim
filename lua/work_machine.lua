869a0397b3b0b66b97dfbe82e8d0770dca6766a3
Philipp Rost <rphilipp@gi-netz.de>
2026-05-13 09:15:39 +0200

    feat: Set up makeprg and shell for windows/python

--- lua/work_machine.lua
+++ lua/work_machine.lua
@@ -2,7 +2,18 @@ IS_WORK_MACHINE = true
 ON_WINDOWS = vim.fn.has('win32')
 
 if IS_WORK_MACHINE and ON_WINDOWS then
-    vim.opt.shell = 'cmd.exe'
+    vim.cmd([[
+        " `h shell-powershell`
+        " Using these two commands from the Help doesnt work, so I replaced them.
+		"let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+		"let &shellcmdflag = '-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';$PSStyle.OutputRendering=''plaintext'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+		let &shell = 'powershell.exe'
+		let &shellcmdflag = '-Command'
+		let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+		let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+		set shellquote= shellxquote=
+    ]])
+
     -- According to https://blog.nikfp.com/how-to-install-and-set-up-neovim-on-windows this might accelerate Neovim on Windows (for me, it doesn't):
     vim.g.nofsync = true
 end
