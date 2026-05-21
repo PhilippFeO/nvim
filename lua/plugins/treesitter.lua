a32056f0ce2d64c3218c65608380ab8b441665db
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2026-05-21 15:32:46 +0200

    feat(treesitter): Changes regarding Neovim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,13 +4,6 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
     'nvim-treesitter/nvim-treesitter-context',
   },
-  -- config = function()
-  --   pcall(require('nvim-treesitter.install').update { with_sync = true })
-  -- end,
-  -- lazy = false,
-  -- build = ':TSUpdate',
-  -- Archived
-  -- main: 0.12
-  -- master: 0.11
-  -- branch = 'main' and vim.fn.has('nvim-0.12') or 'master',
+  lazy = false,
+  build = ':TSUpdate',
 }
