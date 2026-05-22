aac763063e4cafbc1783dccf193ca39c9404d4c3
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2026-05-22 20:22:32 +0200

    feat(keymap): 'gi' default overwritten with LSP implementation

--- after/plugin/lsp-setup.lua
+++ after/plugin/lsp-setup.lua
@@ -87,6 +87,20 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     -- Dont forget LSP default mappings: `h lsp-defaults`
     -- The Vim mapping 'gd' sometimes goes to the first occurence of a symbol, for instance to a location in a comment/docstring coming before the actual declaration, s. `h gd`. Better use `vim.lsp.buf.declaration()`.
+    vim.keymap.set('n', 'gi', function()
+        vim.lsp.buf.implementation()
+        -- Without deferring, centering does not work
+        vim.defer_fn(
+          function()
+            vim.cmd('normal zz')
+          end,
+          50)
+      end,
+      {
+        remap = true,
+        desc = lsp_desc('[g]oto [i]mplementation')
+      }
+    )
     vim.keymap.set('n', 'gdv', 'gdzz',
       {
         remap = true,
