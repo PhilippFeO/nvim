50a7c56643fb7d78ceee46c8d81cab146697145f
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2026-05-27 07:37:35 +0200

    chore: Comments

--- after/plugin/nvim-dap-python.lua
+++ after/plugin/nvim-dap-python.lua
@@ -2,10 +2,7 @@ local dap_python = require('dap-python')
 -- setup according to https://github.com/mfussenegger/nvim-dap-python
 -- The debugger will automatically pick-up another virtual environment if it is activated before neovim is started.
 
--- Either project specific Venv with debugpy installed or ...
--- dap_python.setup(vim.fn.expand(os.getenv('VIRTUAL_ENV') .. '/bin/python3'))
-
--- debugpy specific Venv, ie. a Venv with only debugpy installed
+-- Either project specific Venv with debugpy installed or general Venv with debugpy installed.
 dap_python.setup(vim.g.python3_host_prog)
 
 dap_python.test_runner = 'pytest'
