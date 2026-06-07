b135e62717614151685b30df62df2b9efb5a397f
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2026-06-07 20:14:38 +0200

    🏡 07.06.2026 20:14

--- lua/dap-configs/python-default.lua
+++ lua/dap-configs/python-default.lua
@@ -64,12 +64,11 @@ local default_no_console_foreign_code = {
 local grocery_shopper = {
     console = 'externalTerminal',
     name = "Debug grocery_shopper with '-n 2'",
-    -- needs absolute path
-    -- program = vim.fn.expand '~/programmieren/grocery-shopper/grocery-shopper/start.py',
-    program = vim.fn.expand './grocery-shopper/start.py',
+    program = './grocery_shopper/start.py',
     request = "launch",
     type = "python",
-    -- cwd = vim.fn.expand '~/programmieren/grocery_shopper',
+    -- necessary, doesn't use PWD automatically
+    cwd = vim.fn.expand '~/programmieren/grocery-shopper',
     args = { '-n', '2' }
 }
 
