a57f8e4655409b1609c278fe2c83b5fbb051faf2
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2026-06-09 18:35:05 +0200

    feat(treesitter): Add htmldjango injections for python

---
+++ queries/python/injections.scm
@@ -0,0 +1,12 @@
+; extends
+;; `h treesitter-query-modeline`
+
+; Inject `htmldjango` (and by this `javascript` and `css`) into the string highlighting, if a variable name ends on `_HTML`.
+((assignment
+  left: (identifier) @_name
+  right: (string
+    (string_content) @injection.content))
+  ; #match? compares against a regex pattern
+  (#match? @_name "_HTML$")
+  ; inject the `htmldjango` sytax highlighting
+  (#set! injection.language "htmldjango"))
