c37a6055eeeba44906459098411d31608262ea68
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2026-06-12 21:12:29 +0200

    🏡 12.06.2026 21:12

--- lua/dap-configs/python-set-GPSIFD.lua
+++ lua/dap-configs/python-set-GPSIFD.lua
@@ -1,33 +1,21 @@
 local dap_defaults = {
   request = 'launch',
   type = 'python',
-  cwd = vim.fn.expand('~/programmieren/GPSPos_setzen/'),
   args = { 'jpgs/without.jpg' },
 }
 
-
-local flask_set_GPSIFD = vim.tbl_extend(
+local geocode_photos = vim.tbl_extend(
   'force',
   {
-    program = vim.fn.expand('~/programmieren/GPSPos_setzen/src/flask_set_GPSIFD.py'),
-    name = 'Set GPSIFD with Flask',
-  },
-  dap_defaults
-)
-
-local file_set_GPSIFD = vim.tbl_extend(
-  'force',
-  {
-    program = vim.fn.expand('~/programmieren/GPSPos_setzen/src/file_set_GPSIFD.py'),
-    name = 'Set GPSIFD with File',
+    program = 'geocode.py',
+    name = 'Geocode Photos',
   },
   dap_defaults
 )
 
 return {
   configs = {
-    flask_set_GPSIFD,
-    file_set_GPSIFD,
+    geocode_photos,
   },
   test_configs = {}
 }
